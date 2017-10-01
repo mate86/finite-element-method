2d8f9c271434bfcd79570b2192848e9fbe647617
Balázs Máté <balazsm86@gmail.com>
2017-10-01 10:57:59 +0200

    Creating tests for Force class

--- src/test/java/com.codecool.finite_element_method/ForceTest.java
+++ src/test/java/com.codecool.finite_element_method/ForceTest.java
@@ -1,7 +1,23 @@
 package com.codecool.finite_element_method;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ForceTest {
+    @Test
+    void testCreatingNewForce() {
+        Force force = new Force(10.0, 0.001);
+        assertEquals(10.0, force.getMagnitude());
+        assertEquals(0.001, force.getAngle());
+    }
 
+    @Test
+    void testSettingForce() {
+        Force force = new Force(10.0, 0.001);
+        force.setMagnitude(30.2);
+        force.setAngle(359.9);
+        assertEquals(30.2, force.getMagnitude());
+        assertEquals(359.9, force.getAngle());
+    }
 }
